c4aaa68cb54fade54d531ba035efb4a9476e1f8b
Drew Carlson <awcarlson@protonmail.com>
2021-01-10 00:15:31 -0800

    Jvm cleanup

--- src/iosMain/kotlin/SwiftShims.kt
+++
@@ -1,6 +0,0 @@
-package drewcarlson.walletkit
-
-
-object SwiftShims {
-    fun stringToByteArray(string: String): ByteArray = string.encodeToByteArray()
-}
---
+++ src/jvmCommonMain/kotlin/util.kt
@@ -0,0 +1,40 @@
+package drewcarlson.walletkit
+
+import com.breadwallet.corenative.crypto.BRCryptoSyncStoppedReason
+import com.breadwallet.corenative.crypto.BRCryptoSyncStoppedReasonType.*
+import com.breadwallet.corenative.crypto.BRCryptoWalletManagerDisconnectReason
+import com.breadwallet.corenative.crypto.BRCryptoWalletManagerDisconnectReasonType.*
+import com.breadwallet.corenative.crypto.BRCryptoWalletManagerState
+import com.breadwallet.corenative.crypto.BRCryptoWalletManagerStateType.*
+import drewcarlson.walletkit.SyncStoppedReason.*
+
+
+fun BRCryptoSyncStoppedReason.asApiReason(): SyncStoppedReason =
+        when (type()) {
+            CRYPTO_SYNC_STOPPED_REASON_COMPLETE -> COMPLETE
+            CRYPTO_SYNC_STOPPED_REASON_REQUESTED -> REQUESTED
+            CRYPTO_SYNC_STOPPED_REASON_UNKNOWN -> UNKNOWN
+            CRYPTO_SYNC_STOPPED_REASON_POSIX -> POSIX(u.posix.errnum, message.orNull())
+            else -> error("unknown sync stopped reason")
+        }
+
+
+fun BRCryptoWalletManagerDisconnectReason.asApiReason(): WalletManagerDisconnectReason =
+        when (type()) {
+            CRYPTO_WALLET_MANAGER_DISCONNECT_REASON_REQUESTED -> WalletManagerDisconnectReason.REQUESTED
+            CRYPTO_WALLET_MANAGER_DISCONNECT_REASON_UNKNOWN -> WalletManagerDisconnectReason.UNKNOWN
+            CRYPTO_WALLET_MANAGER_DISCONNECT_REASON_POSIX ->
+                WalletManagerDisconnectReason.POSIX(u.posix.errnum, message.orNull())
+            else -> error("unknown disconnect reason")
+        }
+
+fun BRCryptoWalletManagerState.asApiState(): WalletManagerState =
+        when (type()) {
+            CRYPTO_WALLET_MANAGER_STATE_CREATED -> WalletManagerState.CREATED
+            CRYPTO_WALLET_MANAGER_STATE_DISCONNECTED ->
+                WalletManagerState.DISCONNECTED(u.disconnected.reason.asApiReason())
+            CRYPTO_WALLET_MANAGER_STATE_CONNECTED -> WalletManagerState.CONNECTED
+            CRYPTO_WALLET_MANAGER_STATE_SYNCING -> WalletManagerState.SYNCING
+            CRYPTO_WALLET_MANAGER_STATE_DELETED -> WalletManagerState.DELETED
+            else -> error("Unknown wallet manager state")
+        }
